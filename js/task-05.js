ffc3de6bc269cf370133c2cfac17c05f17857989
AH400796 <A.Hokhman@gmail.com>
2022-11-06 18:07:29 +0200

    6

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,28 @@
-const nameInputRef = document.querySelector("#name-input");
-const nameOutputRef = document.querySelector("#name-output");
+// const nameInputRef = document.querySelector("#name-input");
+// const nameOutputRef = document.querySelector("#name-output");
 
-const onInput = ({ currentTarget }) => {
-  nameOutputRef.textContent = currentTarget.value;
+// const onInput = ({ currentTarget }) => {
+//   nameOutputRef.textContent = currentTarget.value;
 
-  if (!nameOutputRef.textContent) {
-    nameOutputRef.textContent = "Anonymous";
-  }
+//   if (!nameOutputRef.textContent) {
+//     nameOutputRef.textContent = "Anonymous";
+//   }
+// };
+
+// nameInputRef.addEventListener("input", onInput);
+
+const refs = {
+  input: document.querySelector("#name-input"),
+  span: document.querySelector("#name-output"),
 };
 
-nameInputRef.addEventListener("input", onInput);
+refs.input.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  console.log(event.currentTarget.value);
+  refs.span.textContent = event.currentTarget.value;
+
+  if (!refs.span.textContent) {
+    refs.span.textContent = "Anonymous";
+  }
+}
